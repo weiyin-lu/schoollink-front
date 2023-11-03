e544817761948aeb4098d98f6719b79b9b50483d
weiyin-lu <weiyin2002@outlook.com>
2023-11-03 16:59:22 +0800

    完善字典管理功能

--- src/components/DicList.vue
+++ src/components/DicList.vue
@@ -1,5 +1,6 @@
 <template>
-  <el-button v-if="vuex.state.role[0] == 'admin'" type="primary">新增字典</el-button>
+  <el-button v-if="vuex.state.role[0] == 'admin'" type="primary" @click="dialogVisibleAdd = true">新增/修改字典
+  </el-button>
   <div id="pad">
     <el-tag>字典列表</el-tag>
   </div>
@@ -15,7 +16,7 @@
       </el-col>
     </el-row>
   </div>
-  <el-dialog v-model="dialogVisible">
+  <el-dialog v-model="dialogVisibleDisplay">
     <template #title>
       <h3>字典详情</h3>
     </template>
@@ -37,9 +38,15 @@
         <el-button size="small">修改</el-button>
       </el-col>
     </el-row>
-    <template #footer>
-      <el-button type="primary">添加</el-button>
+  </el-dialog>
+  <el-dialog v-model="dialogVisibleAdd">
+    <template #title>
+      <h3>新增字典</h3>
     </template>
+    <el-input placeholder="字典集编码" v-model="dicData.type"/>
+    <el-input placeholder="字典值编码" v-model="dicData.key"/>
+    <el-input placeholder="字典值" v-model="dicData.value"/>
+    <el-button @click="addDic()" type="success">添加</el-button>
   </el-dialog>
 </template>
 
@@ -47,22 +54,46 @@
 
 import {inject, onMounted, ref} from "vue";
 import {useStore} from "vuex";
+import {ElMessage} from "element-plus";
 // 全局组件变量
 const request = inject('$api')
 const vuex = useStore()
 // 数据列表
 const dicKeyList = ref()
 const dicList = ref({})
+const dicData = ref({
+  key: null,
+  type: null,
+  value: null
+})
 // 判断标识和查询条件
-const dialogVisible = ref(false)
+const dialogVisibleDisplay = ref(false)
+const dialogVisibleAdd = ref(false)
 
 // 获取对应字典编码的键值列表
 const getCodeByType = (value) => {
-  dialogVisible.value = true;
+  dialogVisibleDisplay.value = true;
   request.getCode(value)
       .then(r => {
         dicList.value = r.data.data
       })
+}
+// 新增字典
+const addDic = () => {
+  request.addCode(dicData.value)
+      .then(r => {
+        if (r.data.code = 200) {
+          ElMessage.success(r.data.msg)
+          dialogVisibleDisplay.value = false;
+        } else {
+          ElMessage.error(r.data.msg)
+        }
+      })
+  dicData.value = {
+    key: null,
+    type: null,
+    value: null
+  }
 }
 // 初始化
 onMounted(() => {
